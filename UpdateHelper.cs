45440efcc690911dc9f66a986a4077098884815a
laggson <m.estrich98@gmail.com>
2017-01-26 19:20:13 +0100

    IsNewerVersionAvailable and Version-Compare from Updater

--- UpdateHelper.cs
+++ UpdateHelper.cs
@@ -7,6 +7,7 @@ namespace Laggson.Common
 {
     public static class UpdateHelper
     {
+        private const string API_URL = "http://h2608125.stratoserver.net:5000/api/";
         private static string MainDir => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Laggson Softworks";
         private static string UpdateDir => MainDir + @"\Shared";
         
@@ -20,8 +21,47 @@ namespace Laggson.Common
 
         }
 
-        // TODO: Teste diesen shit, yo!
-        // TODO: versions-Datei erstellen
+        public static void InstallAndLaunchUpdater(string name, Version version, string path)
+        {
+            InstallUpdater();
+
+            LaunchUpdater(name, version, path);
+        }
+
+        /// <summary>
+        /// Überprüft die WebApi, ob eine neuere Version der Software verfügbar ist.
+        /// Löst eine Ausnahme aus, falls die Client-Version neuer ist.
+        /// </summary>
+        /// <returns>True, wenn eine neuer Version vorhanden ist. Sonst false</returns>
+        public static bool IsNewVersionAvailable(string name, Version version)
+        {
+            var cl = new WebClient();
+            var result = cl.DownloadString(API_URL + "version/" + name);
+            var newest = new Version(result);
+
+            return newest.IsLargerThan(version);
+        }
+
+        /// <summary>
+        /// Überprüft die WebApi, ob eine neuere Version der Software verfügbar ist.
+        /// Löst eine Ausnahme aus, falls die Client-Version neuer ist.
+        /// </summary>
+        /// <returns>True, wenn eine neuer Version vorhanden ist. Sonst false</returns>
+        public static bool IsNewVersionAvailable(string name, string version)
+        {
+            return IsNewVersionAvailable(name, new Version(version));
+        }
+
+        public static bool IsLargerThan(this Version maybeLargerVer, Version otherVer)
+        {
+            int compared = maybeLargerVer.CompareTo(otherVer);
+
+            if (compared == -1)
+                throw new ArgumentException("Du kannst doch keine neuere Version haben als der Server vallah.");
+
+            return compared == 1;
+        }
+
         private static void RetrieveUpdater()
         {
             string updFilePath = "http://h2608125.stratoserver.net:54157/Updater.exe";
