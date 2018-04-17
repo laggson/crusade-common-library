aac23180886a0038c223c47783f5ef817bdccb61
Markus S <m.estrich98@gmail.com>
2018-04-17 21:23:16 +0200

    4.7.1 und hoffnung auf notifications (klappt td nich)

--- Laggson.Common/Http.cs
+++ Laggson.Common/Http.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 
@@ -12,7 +13,7 @@ namespace Laggson.Common
       /// <typeparam name="T">Der Typ der Objekte, der erwartet wird.</typeparam>
       /// <param name="url">Die Adresse, von der abgerufen werden soll.</param>
       /// <returns>Eine Aufzählung aller gefundener Items.</returns>
-      public static IEnumerable<T> ReadItems<T>(string url)
+      public static IList<T> ReadItems<T>(string url)
       {
          List<T> data;
 
@@ -26,13 +27,42 @@ namespace Laggson.Common
                var response = client.DownloadString(url);
                data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<T>>(response);
             }
-            catch
+            catch (Exception e)
             {
+               if (e is WebException we)
+                  throw we;
+
                data = null;
             }
          }
 
          return data;
+      }
+
+      public static T PostItem<T>(T item, string url)
+      {
+         using (var client = new WebClient())
+         {
+            client.BaseAddress = url;
+            client.Encoding = Encoding.UTF8;
+            client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+
+            try
+            {
+               var json = Newtonsoft.Json.JsonConvert.SerializeObject(item);
+               var result = client.UploadString(url, "POST", json);
+
+               return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(result);
+            }
+            catch (Exception e)
+            {
+               if (e is WebException we)
+                  throw we;
+
+               return default;
+            }
+
+         }
       }
    }
 }
--- Laggson.Common/Notifications/ToastNotifier.cs
+++ Laggson.Common/Notifications/ToastNotifier.cs
@@ -8,171 +8,211 @@ using IWshRuntimeLibrary;
 
 namespace Laggson.Common.Notifications
 {
-   // Größtenteils https://www.whitebyte.info/programming/c/how-to-make-a-notification-from-a-c-desktop-application-in-windows-10
-   public class ToastNotifier
-   {
-      /// <summary>
-      /// Enthält den Namen der Anwendung, die die Toasts verwendet.
-      /// </summary>
-      private static string ApplicationName { get; set; }
-      /// <summary>
-      /// Gibt an, ob die Klasse bereits initialisiert wurde.
-      /// </summary>
-      public static bool IsInitialized { get; private set; }
+    // Größtenteils https://www.whitebyte.info/programming/c/how-to-make-a-notification-from-a-c-desktop-application-in-windows-10
+    public class ToastNotifier
+    {
+        /// <summary>
+        /// Enthält den Namen der Anwendung, die die Toasts verwendet.
+        /// </summary>
+        private static string ApplicationName { get; set; }
+        /// <summary>
+        /// Gibt an, ob die Klasse bereits initialisiert wurde.
+        /// </summary>
+        public static bool IsInitialized { get; private set; }
 
-      /// <summary>
-      /// Initialisiert die Klasse für die angegebene Application.
-      /// </summary>
-      /// <param name="applicationName">Der Name der zu registrierenden Anwendung. (z.B. Microsoft.Samples.DesktopToastsSample)</param>
-      /// <param name="iconPath">Der Pfad zur ICO-Datei, die in der Startmenü-Verknüpfung hinterlegt werden soll.</param>
-      public static void Init(string applicationName, string iconPath = "")
-      {
-         ApplicationName = applicationName;
-         IsInitialized = true;
+        /// <summary>
+        /// Initialisiert die Klasse für die angegebene Application.
+        /// </summary>
+        /// <param name="applicationName">Der Name der zu registrierenden Anwendung. (z.B. Microsoft.Samples.DesktopToastsSample)</param>
+        /// <param name="iconPath">Der Pfad zur ICO-Datei, die in der Startmenü-Verknüpfung hinterlegt werden soll.</param>
+        public static void Init(string applicationName, string iconPath = "")
+        {
+            ApplicationName = applicationName;
+            IsInitialized = true;
 
-         // War mal nicht nötig. 1709 der Sack.
-         TryCreateShortcut(iconPath);
-      }
+            // War mal nicht nötig. 1709 der Sack.
+            TryCreateShortcut(iconPath);
+        }
 
-      /// <summary>
-      /// Versucht, einen Shortcut im Startmenü zu erstellen.
-      /// </summary>
-      private static void TryCreateShortcut(string iconPath)
-      {
-         var shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
-             + "\\Microsoft\\Windows\\Start Menu\\Programs\\Laggson Softworks\\" + ApplicationName + ".lnk"; //
+        /// <summary>
+        /// Versucht, einen Shortcut im Startmenü zu erstellen.
+        /// </summary>
+        private static void TryCreateShortcut(string iconPath)
+        {
+            var shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                + "\\Microsoft\\Windows\\Start Menu\\Programs\\Laggson Softworks\\" + ApplicationName + ".lnk"; //
 
-         var directory = Path.GetDirectoryName(shortcutPath) ?? "";
+            var directory = Path.GetDirectoryName(shortcutPath) ?? "";
 
-         if (!Directory.Exists(directory))
-            Directory.CreateDirectory(directory);
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
 
-         if (!System.IO.File.Exists(shortcutPath))
-         {
-            InstallShortcut(shortcutPath, iconPath);
-         }
-      }
-
-      private static void InstallShortcut(string shortcutPath, string iconPath)
-      {
-         string destinationPath = Process.GetCurrentProcess().MainModule.FileName;
-         string description = "Eine Verknüpfung zu einer Laggson SoftWorks-Produktion.";
-
-         WshShell wsh = new WshShell();
-
-         if (!(wsh.CreateShortcut(shortcutPath) is IWshShortcut shortcut))
-            throw new ArgumentNullException(nameof(wsh));
-
-         shortcut.Arguments = "";
-         shortcut.TargetPath = destinationPath;
-         // not sure about what this is for
-         shortcut.WindowStyle = 1;
-         shortcut.Description = description;
-         shortcut.WorkingDirectory = Path.GetDirectoryName(destinationPath);
-         shortcut.IconLocation = iconPath;
-         shortcut.Save();
-      }
-
-      /// <summary>
-      /// Bastelt sich den <see cref="ToastTemplateType"/> zusammmen, der zur message passt und schmeißt was, wenn die message leer ist.
-      /// </summary>
-      /// <param name="message">Die anzuzeigende Nachricht.</param>
-      /// <exception cref="ArgumentException">Falls der Titel von <paramref name="message"/> leer ist.</exception>
-      /// <exception cref="ArgumentNullException">Falls <paramref name="message"/> null ist.</exception>
-      /// <returns>Den passenden <see cref="ToastTemplateType"/></returns>
-      private static ToastTemplateType GetTemplateTypeForMessage(MessageItem message)
-      {
-         if (message == null)
-            throw new ArgumentNullException(nameof(message), "Brudi, was kaputt mit dir?");
-
-         if (string.IsNullOrEmpty(message.Header))
-            throw new ArgumentException("Der Titel der Nachricht darf nicht leer sein.", nameof(message));
-
-         if (string.IsNullOrEmpty(message.ImagePath))
-         {
-            if (string.IsNullOrEmpty(message.Message1))
+            if (!System.IO.File.Exists(shortcutPath))
             {
-               return ToastTemplateType.ToastText01;
+                InstallShortcut(shortcutPath, iconPath);
+            }
+        }
+
+        private static void InstallShortcut(string shortcutPath, string iconPath)
+        {
+            string destinationPath = Process.GetCurrentProcess().MainModule.FileName;
+            string description = "Eine Verknüpfung zu einer Laggson SoftWorks-Produktion.";
+
+            WshShell wsh = new WshShell();
+
+            if (!(wsh.CreateShortcut(shortcutPath) is IWshShortcut shortcut))
+                throw new ArgumentNullException(nameof(wsh));
+
+            shortcut.Arguments = "";
+            shortcut.TargetPath = destinationPath;
+            // not sure about what this is for
+            shortcut.WindowStyle = 1;
+            shortcut.Description = description;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(destinationPath);
+            shortcut.IconLocation = iconPath;
+            shortcut.Save();
+        }
+
+        /// <summary>
+        /// Bastelt sich den <see cref="ToastTemplateType"/> zusammmen, der zur message passt und schmeißt was, wenn die message leer ist.
+        /// </summary>
+        /// <param name="message">Die anzuzeigende Nachricht.</param>
+        /// <exception cref="ArgumentException">Falls der Titel von <paramref name="message"/> leer ist.</exception>
+        /// <exception cref="ArgumentNullException">Falls <paramref name="message"/> null ist.</exception>
+        /// <returns>Den passenden <see cref="ToastTemplateType"/></returns>
+        private static ToastTemplateType GetTemplateTypeForMessage(MessageItem message)
+        {
+            if (message == null)
+                throw new ArgumentNullException(nameof(message), "Brudi, was kaputt mit dir?");
+
+            if (string.IsNullOrEmpty(message.Header))
+                throw new ArgumentException("Der Titel der Nachricht darf nicht leer sein.", nameof(message));
+
+            if (string.IsNullOrEmpty(message.ImagePath))
+            {
+                if (string.IsNullOrEmpty(message.Message1))
+                {
+                    return ToastTemplateType.ToastText01;
+                }
+                else
+                {
+                    return string.IsNullOrEmpty(message.Message2)
+                       ? ToastTemplateType.ToastText02
+                       : ToastTemplateType.ToastText04;
+                }
             }
             else
             {
-               return string.IsNullOrEmpty(message.Message2) 
-                  ? ToastTemplateType.ToastText02 
-                  : ToastTemplateType.ToastText04;
+                if (string.IsNullOrEmpty(message.Message1))
+                {
+                    return ToastTemplateType.ToastImageAndText01;
+                }
+                else
+                {
+                    return string.IsNullOrEmpty(message.Message2)
+                       ? ToastTemplateType.ToastImageAndText02
+                       : ToastTemplateType.ToastImageAndText04;
+                }
             }
-         }
-         else
-         {
-            if (string.IsNullOrEmpty(message.Message1))
+        }
+
+        public static void TestNeueMethode()
+        {
+            /**
+            var t2 = "<toast>" + 
+                "<visual>" + 
+                  "<binding template=\"ToastImageAndText04\">" + 
+                    "<image id=\"1\" src=\"C:\\Users\\marku\\AppData\\Local\\Temp\\TimeTrack\\icon.png\"/>" + 
+                    "<text id=\"1\">Guten Morgen!</text>" + 
+                    "<text id=\"2\">Beginn heute: 10:13</text>" + 
+                    "<text id=\"3\">5 Einträge wurden geladen.</text>" + 
+                  "</binding>" + 
+                "</visual>" + 
+              "</toast>";
+    */
+
+            var template = "<toast launch=\"app-defined-string\">" +
+                "<visual>" +
+                 "<binding template =\"ToastGeneric\">" +
+                  "<text>Ich bin ein Beispiel</text>" +
+                  "<text>Kannst du das hier lesen?</text>" +
+                 "</binding>" +
+                "</visual>" +
+                "<actions>" +
+                 "<action activationType=\"background\" content=\"Ja\" arguments=\"yes\"/>" +
+                 "<action activationType=\"background\" content=\"Nein\" arguments=\"no\"/>" +
+                "</actions>" +
+               "</toast>";
+
+            var xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(template);
+
+            var toastNotification = new ToastNotification(xmlDocument);
+            var notification = ToastNotificationManager.CreateToastNotifier(ApplicationName);
+
+            notification.Show(toastNotification);
+        }
+
+        /// <summary>
+        /// Zeigt eine Windows-10-Toast-Notification mit dem Inhalt der angegebenen <paramref name="message"/> aus.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Falls nicht vorher die <see cref="Init(string,string)"/>-Methode aufgerufen wurde.</exception>
+        /// <param name="message">Die anzuzeigende Nachricht.</param>
+        public static void Show(MessageItem message)
+        {
+            if (!IsInitialized)
+                throw new InvalidOperationException("Du musst zuerst die Init-Methode callen.");
+
+            //TestNeueMethode();
+            //return;
+
+            var templateType = GetTemplateTypeForMessage(message);
+
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(templateType);
+
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+
+            string templateString = templateType.ToString();
+            int range = int.Parse(templateString.Last().ToString());
+            stringElements[0].AppendChild(toastXml.CreateTextNode(message.Header));
+
+            if (range >= 2)
+                stringElements[1].AppendChild(toastXml.CreateTextNode(message.Message1));
+
+            if (range >= 4)
+                stringElements[2].AppendChild(toastXml.CreateTextNode(message.Message2));
+
+            if (!string.IsNullOrEmpty(message.ImagePath))
             {
-               return ToastTemplateType.ToastImageAndText01;
+                XmlNodeList imageElements = toastXml.GetElementsByTagName("image");
+                imageElements[0].Attributes.GetNamedItem("src").NodeValue = message.ImagePath;
             }
-            else
-            {
-               return string.IsNullOrEmpty(message.Message2)
-                  ? ToastTemplateType.ToastImageAndText02
-                  : ToastTemplateType.ToastImageAndText04;
-            }
-         }
-      }
 
-      /// <summary>
-      /// Zeigt eine Windows-10-Toast-Notification mit dem Inhalt der angegebenen <paramref name="message"/> aus.
-      /// </summary>
-      /// <exception cref="InvalidOperationException">Falls nicht vorher die <see cref="Init(string,string)"/>-Methode aufgerufen wurde.</exception>
-      /// <param name="message">Die anzuzeigende Nachricht.</param>
-      public static void Show(MessageItem message)
-      {
-         if (!IsInitialized)
-            throw new InvalidOperationException("Du musst zuerst die Init-Methode callen.");
+            ToastNotification toast = new ToastNotification(toastXml);
+            toast.Activated += Toast_Activated;
+            toast.Dismissed += Toast_Dismissed;
+            toast.Failed += Toast_Failed;
 
-         var templateType = GetTemplateTypeForMessage(message);
+            ToastNotificationManager.CreateToastNotifier(ApplicationName).Show(toast);
+        }
 
-         XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(templateType);
+        public static event EventHandler ToastClicked;
+        public static event DismissedEventHandler ToastDismissed;
+        public static event FailedEventHandler ToastFailed;
 
-         XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+        private static void Toast_Activated(ToastNotification sender, object args)
+        {
+            ToastClicked?.Invoke(sender, EventArgs.Empty);
+        }
 
-         string templateString = templateType.ToString();
-         int range = int.Parse(templateString.Last().ToString());
-         stringElements[0].AppendChild(toastXml.CreateTextNode(message.Header));
+        private static void Toast_Dismissed(ToastNotification sender, ToastDismissedEventArgs args)
+        {
+            ToastDismissed?.Invoke(sender, (ToastDismissalReason)(int)args.Reason);
+        }
 
-         if (range >= 2)
-            stringElements[1].AppendChild(toastXml.CreateTextNode(message.Message1));
-
-         if (range >= 4)
-            stringElements[2].AppendChild(toastXml.CreateTextNode(message.Message2));
-
-         if (!string.IsNullOrEmpty(message.ImagePath))
-         {
-            XmlNodeList imageElements = toastXml.GetElementsByTagName("image");
-            imageElements[0].Attributes.GetNamedItem("src").NodeValue = message.ImagePath;
-         }
-
-         ToastNotification toast = new ToastNotification(toastXml);
-         toast.Activated += Toast_Activated;
-         toast.Dismissed += Toast_Dismissed;
-         toast.Failed += Toast_Failed;
-
-         ToastNotificationManager.CreateToastNotifier(ApplicationName).Show(toast);
-      }
-
-      public static event EventHandler ToastClicked;
-      public static event DismissedEventHandler ToastDismissed;
-      public static event FailedEventHandler ToastFailed;
-
-      private static void Toast_Activated(ToastNotification sender, object args)
-      {
-         ToastClicked?.Invoke(sender, EventArgs.Empty);
-      }
-
-      private static void Toast_Dismissed(ToastNotification sender, ToastDismissedEventArgs args)
-      {
-         ToastDismissed?.Invoke(sender, (ToastDismissalReason) (int) args.Reason);
-      }
-
-      private static void Toast_Failed(ToastNotification sender, ToastFailedEventArgs args)
-      {
-         ToastFailed?.Invoke(sender, args.ErrorCode);
-      }
-   }
+        private static void Toast_Failed(ToastNotification sender, ToastFailedEventArgs args)
+        {
+            ToastFailed?.Invoke(sender, args.ErrorCode);
+        }
+    }
 }
